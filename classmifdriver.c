40824f5ac4e00770353630844591bdfa475a5cbc
Classmif <trackadoor@gmail.com>
2025-03-17 14:27:22 +0200

    Update classmifdriver.c
    /*
    ** 2025-04-17
    **
    ** The author disclaims copyright to this source code.  In place of
    ** a legal notice, here is a blessing:
    **
    **    May you do good and not evil.
    **    May you find forgiveness for yourself and forgive others.
    **    May you share freely, never taking more than you give.
    **
    */

--- classmifdriver.c
+++ classmifdriver.c
@@ -1,3 +1,16 @@
+/*
+** 2025-04-17
+**
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+**
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
