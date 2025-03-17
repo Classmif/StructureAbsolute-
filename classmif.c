63596cc5e9e7687d149c94c0199a797440998217
Classmif <trackadoor@gmail.com>
2025-03-17 14:26:32 +0200

    Update classmif.c
    /*
    ** 2025-04-17
    **
    ** The author disclaims copyright to this source code.  In place of
    ** a legal notice, here is a blessing:
    **
    **    May you do good and not evil.
    **    May you find forgiveness for yourself and forgive others.
    **    May you share freely, never taking more than you give.
    **
    */

--- classmif.c
+++ classmif.c
@@ -1,3 +1,15 @@
+/*
+** 2025-04-17
+**
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+**
+*/
+
 #include <stdio.h>
 
 typedef struct {
